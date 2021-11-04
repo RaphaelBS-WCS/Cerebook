4299050fe702226f07f1da3b8105c34c48df2edb
Niurwa <raphaelbilletservoin@gmail.com>
2021-11-04 15:58:02 +0100

    Initial Commit

---
+++ src/main/java/com/wildcodeschool/cerebook/CerebookApplication.java
@@ -0,0 +1,14 @@
+package com.wildcodeschool.cerebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class CerebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CerebookApplication.class, args);
+    }
+
+}
