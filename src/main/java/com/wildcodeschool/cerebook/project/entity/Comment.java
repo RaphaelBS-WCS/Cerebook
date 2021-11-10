f178145329cab8ceeeec82972b08d933b639302b
Aenori <nrousset@gmail.com>
2021-11-10 10:36:35 +0100

    Update Comment.java

--- src/main/java/com/wildcodeschool/cerebook/project/entity/Comment.java
+++ src/main/java/com/wildcodeschool/cerebook/project/entity/Comment.java
@@ -12,10 +12,13 @@ public class Comment {
     private String content;
 
     @ManyToOne
+    // NRO 2021-11-10 : good example of why you should not use JoinColumn by
+    //  default :-) There is a spelling mistake
     @JoinColumn(name = "cerebook_uder_id")
     private CerebookUser author;
 
     @ManyToOne(cascade = CascadeType.REFRESH)
+    // NRO 2021-11-10 : Just remove this useless annotation
     @JoinColumn(name = "post_id")
     private Post postId;
 
