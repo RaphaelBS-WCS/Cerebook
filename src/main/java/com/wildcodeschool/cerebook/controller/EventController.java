e062caa595c5d544ad36425b5667a59990c51c10
Gersey Stelmach <gerseystelmach@gmail.com>
2021-12-03 17:49:10 +0100

    solution for modifying event

--- src/main/java/com/wildcodeschool/cerebook/controller/EventController.java
+++ src/main/java/com/wildcodeschool/cerebook/controller/EventController.java
@@ -7,6 +7,7 @@ import com.wildcodeschool.cerebook.repository.EventRepository;
 import com.wildcodeschool.cerebook.repository.UserRepository;
 import com.wildcodeschool.cerebook.service.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.stereotype.Controller;
@@ -16,18 +17,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import javax.validation.Valid;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 import java.sql.Date;
-import java.time.LocalTime;
 import java.util.Objects;
 
 @Controller
@@ -50,10 +43,88 @@ public class EventController {
         String username = principal.getName();
         User currentUser = userRepository.getUserByUsername(username);
 
-        model.addAttribute("eventsByUser", eventRepository.getAllEventsByCreator(currentUser));
+        try {
+            model.addAttribute("eventsByUser", eventRepository.getAllEventsByCreator(currentUser));
+        } catch (Exception e) {
+            return "redirect:/events";
+        }
+
 
         return "events/eventsByUser";
     }
+
+    @GetMapping("/show/{eventId}")
+    public String showEventsById(@ModelAttribute Event event, Model model, Principal principal, @PathVariable Long eventId) {
+
+        String username = principal.getName();
+        User currentUser = userRepository.getUserByUsername(username);
+
+        try {
+            model.addAttribute("eventByIdAndUser", eventRepository.getEventByIdAndByCreator(currentUser, eventId));
+        } catch (Exception e) {
+            return "events/showEventById";
+        }
+
+        return "events/showEventById";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String updateEvent(@PathVariable("id") Long id, @ModelAttribute Event event, Model model, Principal principal) {
+        String username = principal.getName();
+        User currentUser = userRepository.getUserByUsername(username);
+
+        model.addAttribute("eventByIdAndUser", eventRepository.getEventByIdAndByCreator(currentUser, id));
+        model.addAttribute("eventCategories", eventCategoryRepository.getAllEventCategories());
+        return "events/update";
+    }
+
+
+    @PostMapping("/update/{id}")
+    public String modifyEvent(@PathVariable("id") Long id,
+                              @Valid @ModelAttribute Event event,
+                              Model model,
+                              Principal principal,
+                              @RequestParam("date") Date eventDate,
+                              BindingResult bindingResult,
+                              @RequestParam("backgroundPhotoFile") MultipartFile multipartFile) throws ServletException, IOException
+    {
+
+        Event currentEvent = eventRepository.getEventById(id);
+
+     if (!principal.getName().equals(currentEvent.getCreator().getUsername())) {
+         throw new IOException("User not allowed.");
+     }
+        String username = principal.getName();
+        User currentUser = userRepository.getUserByUsername(username);
+
+        long millis = System.currentTimeMillis();
+        Date localDate = new Date(millis);
+
+        model.addAttribute("eventByIdAndUser", eventRepository.getEventByIdAndByCreator(currentUser, id));
+        model.addAttribute("eventCategories", eventCategoryRepository.getAllEventCategories());
+
+
+        if (eventDate.before(localDate)) {
+            bindingResult.rejectValue("date", "error.date", "The event date cannot be before the current date.");
+        }
+        if (bindingResult.hasErrors()) {
+            return "events/update";
+        } else {
+            event.setCreatedAt(currentEvent.getCreatedAt());
+            String fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+            event.setBackgroundPhoto(fileName);
+            event.setCreator(currentUser);
+            event.setUpdatedAt(localDate);
+            Event updatedEvent = eventRepository.save(event);
+            String uploadDir = "src/main/resources/public/images/WebContent/events-uploaded-files/" + updatedEvent.getId();
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
+            return "redirect:/events/show/" +  id;
+        }
+
+    }
+
 
     @GetMapping("/create")
     public String createEvent(Model model) {
@@ -63,14 +134,19 @@ public class EventController {
     }
 
     @PostMapping("/create")
-    public String saveEvent(@Valid @ModelAttribute Event event, Model model, BindingResult bindingResult, Principal principal, @RequestParam("date") Date eventDate, @RequestParam("backgroundPhotoFile") MultipartFile multipartFile) throws ServletException, IOException {
+    public String saveEvent(@Valid @ModelAttribute Event event,
+                            Model model,
+                            BindingResult bindingResult,
+                            Principal principal,
+                            @RequestParam("date") Date eventDate,
+                            @RequestParam("backgroundPhotoFile") MultipartFile multipartFile) throws ServletException, IOException {
 
         long millis = System.currentTimeMillis();
         Date localDate = new Date(millis);
 
         model.addAttribute("eventCategories", eventCategoryRepository.getAllEventCategories());
 
-        if (eventDate.before(localDate) ) {
+        if (eventDate.before(localDate)) {
             bindingResult.rejectValue("date", "error.date", "The event date cannot be before the current date.");
         }
 
@@ -83,20 +159,16 @@ public class EventController {
             String username = principal.getName();
             User currentUser = userRepository.getUserByUsername(username);
             event.setCreator(currentUser);
-       /*     Getting the name of the uploaded file and persisting it to the database */
+            /*     Getting the name of the uploaded file and persisting it to the database */
             String fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
-
             event.setBackgroundPhoto(fileName);
 
             Event savedEvent = eventRepository.save(event);
-
             String uploadDir = "src/main/resources/public/images/WebContent/events-uploaded-files/" + savedEvent.getId();
-
             FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 
             return "redirect:/events";
         }
-
     }
 
 }
